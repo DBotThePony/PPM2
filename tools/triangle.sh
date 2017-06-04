319fbc0e010b96203ac0ade8184fe724ddfa8d26
DBot <roboderpy@gmail.com>
2017-06-04 22:10:01 +0700

    Valid triangles

--- tools/triangle.sh
+++ tools/triangle.sh
@@ -2,7 +2,7 @@
 magick montage \
 '(' '-size' '100x100' 'xc:gold' -fill black -draw "path 'M 0,100  L 50,0  L 100,100 L 0,100 Z' " ')' \
 '(' '-size' '100x100' 'xc:black' -fill gold -draw "path 'M 0,0  L 50,100  L 100,0 L 0,0 Z' " ')' \
--geometry '-24.9+0' \
+-geometry '-24+0' \
 -clone 1 -clone 0 \
 -clone 0 -clone 1 \
 -clone 1 -clone 0 \
@@ -26,3 +26,6 @@ magick montage 'temp/triangle_base.png' \
 -geometry +0+0 \
 -tile 9x \
 'temp/triangle_texture.png'
+
+magick convert -alpha 'set' 'temp/triangle_texture.png' -transparent 'gold' -channel RGB +level-colors white 'temp/triangle_texture_1.png'
+magick convert -alpha 'set' 'temp/triangle_texture.png' -transparent 'black' -channel RGB +level-colors white 'temp/triangle_texture_2.png'
